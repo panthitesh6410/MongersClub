73c8c76b46bee8376ba43bc5d9b25df8cbe3a67a
panthitesh6410 <panthitesh6410@gmail.com>
2019-11-08 08:30:04 +0530

    minor change

--- index.php
+++ index.php
@@ -135,7 +135,7 @@ if(!isset($_POST['show_products']))
       $show_product_owner_id = $data['product_owner_id'];
   
       echo "
-      <div class='card mb-3 ' style='max-width: 800px; height:250px;position: relative;left:15%;box-shadow: 10px 10px 10px 10px #ddd;'>
+      <div class='card' style='max-width: 800px; height:250px;position: relative;left:15%;box-shadow: 10px 10px 10px 10px #ddd;'>
       <div class='row no-gutters'>
         <div class='col-md-4'>
           <img src='product_images/$show_product_image' class='card-img' alt='' style='height:250px;'>
