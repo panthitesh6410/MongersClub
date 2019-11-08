ad2cecd6c3cdd471b3cf7bd86689989eb5a252b1
panthitesh6410 <panthitesh6410@gmail.com>
2019-11-08 16:03:04 +0530

    admin protal front-end complete

--- admin.php
+++ admin.php
@@ -20,45 +20,131 @@
             left: 40%;
             top: 50%;
         }
+        B{
+            COLOR: #000000;
+            FONT-SIZE: 30px;
+        }
+        table{
+            margin-left:30px;
+        }
+        table tr th{
+            padding:20px;
+        }
+        table tr td{
+            padding:20px;
+        }
     </style>
 </head>
 <body>
     
     <div class="row" >
-        <div class="col-sm-4 text-center alert alert-primary"><a href="#" onclick="view1()">Users</a></div>
-        <div class="col-sm-4 text-center alert alert-warning"><a href="#" onclick="view2()">Products</a></div>
-        <div class="col-sm-4 text-center alert alert-secondary"><a href="#" onclick="view3()">Reviews</a></div>
+        <div class="col-sm-4 text-center alert alert-primary "><a href="#" onclick="view1()"><b>USERS</b></a></div>
+        <div class="col-sm-4 text-center alert alert-warning"><a href="#" onclick="view2()"><B>PRODUCTS</B></a></div>
+        <div class="col-sm-4 text-center alert alert-secondary"><a href="#" onclick="view3()"><B>REVIEW</B></a></div>
     </div>
 
         <h1 id="h1">SELECT AN OPTION</h1>
 
     <div class="row alert alert-primary" id="users">
-        <?php 
-            echo "<div class='col-sm-9'>User content</div>
-            <div class='col-sm-1'><button class='btn btn-success'>Insert</button></div>
-            <div class='col-sm-1'><button class='btn btn-warning'>Update</button></div>
-            <div class='col-sm-1'><button class='btn btn-danger'>Delete</button></div>";
-        ?>
+        <table border=5>
+            <tr>
+                <th>USERNAME</th>
+                <th>E-MAIL</th>
+                <th>PASSWORD</th>
+                <th>PHONE</th>
+                <th>INSTITUTE</th>
+                <th>PLACE</th>
+                <th>GENDER</th>
+            </tr>
+            <?php
+                $user_query = "select * from users";
+                $run_user_query = mysqli_query($con, $user_query);
+                while($user_data = mysqli_fetch_array($run_user_query))
+                {
+                    $username = $user_data['user_name'];
+                    $useremail = $user_data['user_email'];
+                    $userpassword = $user_data['user_password'];
+                    $userphone = $user_data['user_phone'];
+                    $userinstitute = $user_data['user_institute'];
+                    $userplace = $user_data['user_place'];
+                    $usergender = $user_data['user_gender'];
+                    echo "
+                    <tr>
+                        <td>$username</td>
+                        <td>$useremail</td>
+                        <td>$userpassword</td>
+                        <td>$userphone</td>
+                        <td>$userinstitute</td>
+                        <td>$userplace</td>
+                        <td>$usergender</td>
+                        <td><button class='btn btn-primary'>UPDATE</button></td>
+                        <td><button class='btn btn-danger'>DELETE</button></td>
+                    </tr>
+                    ";
+                }
+            ?>
+            
+        </table>
     </div>
     <div class="row alert alert-warning" id="products">
-        <?php 
-            echo "<div class='col-sm-9'>Product content</div>
-            <div class='col-sm-1'><button class='btn btn-success'>Insert</button></div>
-            <div class='col-sm-1'><button class='btn btn-warning'>Update</button></div>
-            <div class='col-sm-1'><button class='btn btn-danger'>Delete</button></div>";
-        ?>
+    <table border=5>
+            <tr>
+                <th>PRODUCT NAME</th>
+                <th>PRODUCT MODE</th>
+                <th>PRODUCT DESCRIPTION</th>
+                <th>UPLOAD DATE</tH>
+            </tr>
+            <?php
+                $product_query = "select * from products";
+                $run_product_query = mysqli_query($con, $product_query);
+                while($product_data = mysqli_fetch_array($run_product_query))
+                {
+                    $pro_name = $product_data['product_name'];
+                    $pro_mode = $product_data['product_mode'];
+                    $pro_description = $product_data['product_description'];
+                    $pro_time = $product_data['product_time'];
+                    echo "
+                    <tr>
+                        <td>$pro_name</td>
+                        <td>$pro_mode</td>
+                        <td>$pro_description</td>
+                        <td>$pro_time</td>
+                        <td><button class='btn btn-primary'>UPDATE</button></td>
+                        <td><button class='btn btn-danger'>DELETE</button></td>
+                    </tr>
+                    ";
+                }
+            ?>
+        </table>
     </div>
     <div class="row alert alert-secondary" id="reviews">
-        <?php 
-            echo "<div class='col-sm-9'>Review content</div>
-            <div class='col-sm-1'><button class='btn btn-success'>Insert</button></div>
-            <div class='col-sm-1'><button class='btn btn-warning'>Update</button></div>
-            <div class='col-sm-1'><button class='btn btn-danger'>Delete</button></div>";
-        ?>
+    <table border=5>
+            <tr>
+                <th>REVIEW CONTENT</th>
+                <th>UPLOAD DATE</tH>
+            </tr>
+            <?php
+                $review_query = "select * from reviews";
+                $run_review_query = mysqli_query($con, $review_query);
+                while($review_data = mysqli_fetch_array($run_review_query))
+                {
+                    $review_content = $review_data['review_body'];
+                    $review_time = $review_data['review_date'];
+                    echo "
+                    <tr>
+                        <td>$review_content</td>
+                        <td>$review_time</td>
+                        <td><button class='btn btn-primary'>UPDATE</button></td>
+                        <td><button class='btn btn-danger'>DELETE</button></td>
+                    </tr>
+                    ";
+                }
+            ?>
     </div>
 
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
+
     <script>
         $(document).ready(function(){
             $("body").animate({opacity:"1"}, 2500);
